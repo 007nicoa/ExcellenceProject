73e378e48c551e5a9f792d5a1049c89a72ebd9d1
nardila23 <85463821+nardila23@users.noreply.github.com>
2023-07-17 07:16:17 -0500

    First commit

---
+++ src/FinalProject.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.util.Random;
+
+
+public class FinalProject {
+
+    public static void main(String[] args) {
+        StdDraw.setScale(-2.5, +2.5);
+        StdDraw.enableDoubleBuffering();
+
+
+    }
+
+}
